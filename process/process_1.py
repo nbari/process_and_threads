b09f25d4c7fea6f14ac15c017b6d2b2ca90b1b30
nbari <nbari@dalmp.com>
2013-08-24 10:54:56 +0100

    modified:   process_1.py   modified:   ../threads/thread_1.py

--- process/process_1.py
+++ process/process_1.py
@@ -1,13 +1,21 @@
+import time
 from multiprocessing import Pool
 
 
 def myfunc(i):
     print "sleeping 5 sec from process %d" % i
     time.sleep(3)
+    return i
+
+def log_result(rs):
     print "finised sleeping from process %d" % i
 
 if __name__ == '__main__':
+    start_time = time.time()
     p = Pool()
     for i in range(10):
-        p.apply_async(myfunc, i)
+        p.apply_async(myfunc, args=(i,), callback=log_result)
+    p.close()
     p.join()
+
+    print '\n' + 'Elapsed time: ' + str(time.time() - start_time)
--- threads/thread_1.py
+++ threads/thread_1.py
@@ -8,5 +8,8 @@ def myfunc(i):
     print "finished sleeping from thread %d" % i
 
 for i in range(10):
+    start_time = time.time()
     t = Thread(target=myfunc, args=(i,))
     t.start()
+    #t.join()
+    #print '\n' + 'Elapsed time: ' + str(time.time() - start_time)
